11b1e91553d12e809de49308569b6969adb0370a
Sérgio Ugoline Junior <sergiouine@gmail.com>
2025-09-19 21:03:10 -0300

    CRUD Clientes Completo

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -12,7 +12,48 @@ class Cliente(db.Model):
 
 @app.route("/")
 def home():
-    return "Ola Mundo"
+    return render_template("home.html")
+
+@app.route("/clientes/cadastrar", methods=["GET", "POST"])
+def cadastrar_cliente():
+    if request.method == "POST":
+        nome_cliente = request.form["nome"]
+        telefone_cliente = request.form["telefone"]
+        novo_cliente = Cliente(nome=nome_cliente, telefone_celular=telefone_cliente)
+        db.session.add(novo_cliente)
+        db.session.commit()
+        return redirect(url_for("home"))
+
+    return render_template("cadastrar_cliente.html")
+
+@app.route("/clientes/deletar/<int:id>")
+def deletar_cliente(id):
+    cliente_a_deletar = Cliente.query.get(id)
+    db.session.delete(cliente_a_deletar)
+    db.session.commit()
+    return redirect(url_for("listar_clientes"))
+
+@app.route("/clientes/editar/<int:id>", methods=["GET", "POST"])
+def editar_cliente(id):
+    cliente_a_editar = Cliente.query.get_or_404(id)
+    if request.method == "POST": 
+        nome_cliente = request.form["nome"]
+        telefone_cliente = request.form["telefone"]
+        cliente_a_editar.nome = nome_cliente
+        cliente_a_editar.telefone_celular = telefone_cliente
+        db.session.commit()
+
+        return redirect(url_for("listar_clientes"))
+    
+    return render_template("editar_cliente.html", cliente_a_editar = cliente_a_editar)
+    
+
+@app.route("/clientes")
+def listar_clientes():
+    clientes = Cliente.query.all()
+
+    return render_template("listar_clientes.html", clientes = clientes)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
