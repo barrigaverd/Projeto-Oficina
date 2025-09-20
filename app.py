ba9dad33af92bbafddda9ced6b44dccf402e3259
barrigaverd <sergiouine@gmail.com>
2025-09-20 11:20:38 -0300

    criado classe ordemservico - flask shell

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
@@ -9,6 +10,22 @@ class Cliente(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     nome = db.Column(db.String(100), nullable=False)
     telefone_celular = db.Column(db.String(20), nullable = False)
+
+class OrdemServico(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    numero_os = db.Column(db.Integer, nullable = False)
+    cliente_id = db.Column(db.Integer, db.ForeignKey('cliente.id'), nullable = False)
+    validade_do_orcamento = db.Column(db.String(100))
+    prazo_de_execucao = db.Column(db.String(100))
+    marca = db.Column(db.String(100))
+    modelo = db.Column(db.String(100))
+    equipamento = db.Column(db.String(150), nullable = False)
+    numero_de_serie = db.Column(db.String(100))
+    defeito = db.Column(db.Text, nullable = False)
+    observacoes = db.Column(db.Text)
+    status = db.Column(db.String(50), nullable = False)
+    valor_total = db.Column(db.Float)
+    data_de_criacao = db.Column(db.DateTime, nullable = False, default = datetime.now)
 
 @app.route("/")
 def home():
@@ -51,8 +68,13 @@ def editar_cliente(id):
 @app.route("/clientes")
 def listar_clientes():
     clientes = Cliente.query.all()
-
     return render_template("listar_clientes.html", clientes = clientes)
+
+@app.route("/cliente/<int:id>")
+def detalhes_cliente(id):
+    cliente_a_detalhar = Cliente.query.get(id)
+    return render_template("detalhes_cliente.html", cliente_a_detalhar = cliente_a_detalhar)
+
 
 
 if __name__ == "__main__":
