2d2ee507b1768208ae39ded2fad6af80cc19aefc
Sérgio Ugoline Junior <sergiouine@gmail.com>
2025-09-21 10:55:23 -0300

    sistema de login pronto

--- app.py
+++ app.py
@@ -1,11 +1,45 @@
 from http import client
+import re
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
 from datetime import datetime
+import click
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '0625fa577ac24b41fd655e4935191fb6'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+@app.cli.command("create-user")  # Define o nome do comando no terminal
+@click.argument("username")      # Define o primeiro argumento que o comando espera
+@click.argument("password")      # Define o segundo argumento
+def create_user(username, password):
+    user = Usuario.query.filter_by(username=username).first()
+    if user:
+        print("Erro: Usuário já existe!")
+        return
+    else:
+        password_hash = bcrypt.generate_password_hash(password)
+        novo_usuario = Usuario(username = username, password_hash = password_hash)
+        db.session.add(novo_usuario)
+        db.session.commit()
+        print(f"Usuário {username} criado com sucesso!")
+
+@app.cli.command("delete-user")  # Define o nome do comando no terminal
+@click.argument("username")      # Define o primeiro argumento que o comando espera
+def delete_user(username):
+    usuario_a_deletar = Usuario.query.filter_by(username=username).first()
+    if usuario_a_deletar:
+        db.session.delete(usuario_a_deletar)
+        db.session.commit()
+        print(f"Usuário {username} foi deletado com sucesso!")
+    else:
+        print(f"Usuario {username} não encontrado!")
 
 class Cliente(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -29,11 +63,40 @@ class OrdemServico(db.Model):
     #valor_total = db.Column(db.Float)
     data_de_criacao = db.Column(db.DateTime, nullable = False, default = datetime.now)
 
+class Usuario(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(80), nullable = False, unique = True)
+    password_hash = db.Column(db.String(128), nullable = False)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Usuario.query.get(int(user_id))
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
+
+@app.route("/login", methods = ("GET", "POST"))
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        user = Usuario.query.filter_by(username=username).first()
+
+        if user and bcrypt.check_password_hash(user.password_hash, password):
+            login_user(user)
+            return redirect(url_for('home'))
+
+    return render_template('login.html')
+
 @app.route("/")
 def home():
     return render_template("home.html")
 
 @app.route("/clientes/cadastrar", methods=["GET", "POST"])
+@login_required
 def cadastrar_cliente():
     if request.method == "POST":
         nome_cliente = request.form["nome"]
@@ -46,6 +109,7 @@ def cadastrar_cliente():
     return render_template("cadastrar_cliente.html")
 
 @app.route("/clientes/deletar/<int:id>")
+@login_required
 def deletar_cliente(id):
     cliente_a_deletar = Cliente.query.get(id)
     db.session.delete(cliente_a_deletar)
@@ -53,6 +117,7 @@ def deletar_cliente(id):
     return redirect(url_for("listar_clientes"))
 
 @app.route("/clientes/editar/<int:id>", methods=["GET", "POST"])
+@login_required
 def editar_cliente(id):
     cliente_a_editar = Cliente.query.get_or_404(id)
     if request.method == "POST": 
@@ -68,17 +133,20 @@ def editar_cliente(id):
     
 
 @app.route("/clientes")
+@login_required
 def listar_clientes():
     clientes = Cliente.query.all()
     return render_template("listar_clientes.html", clientes = clientes)
 
 @app.route("/cliente/<int:id>")
+@login_required
 def detalhes_cliente(id):
     cliente_a_detalhar = Cliente.query.get(id)
     ordens_servico = cliente_a_detalhar.ordens_servico
     return render_template("detalhes_cliente.html", cliente_a_detalhar = cliente_a_detalhar, ordens_servico = ordens_servico)
 
 @app.route("/cliente/<int:cliente_id>/os/cadastrar", methods = ["GET", "POST"])
+@login_required
 def cadastrar_os(cliente_id):
     cliente = Cliente.query.get_or_404(cliente_id)
     if request.method == "POST":
@@ -103,6 +171,7 @@ def cadastrar_os(cliente_id):
     return render_template("cadastrar_os.html", cliente = cliente)
 
 @app.route("/os/<int:id>", methods=["GET", "POST"])
+@login_required
 def detalhes_os(id):
     ordem_servico = OrdemServico.query.get(id)
     if request.method == "POST":
@@ -125,6 +194,7 @@ def detalhes_os(id):
     return render_template("detalhes_os.html", ordem_servico = ordem_servico)
 
 @app.route("/os/deletar/<int:id>")
+@login_required
 def deletar_os(id):
     os_a_deletar = OrdemServico.query.get(id)
     id_do_cliente = os_a_deletar.cliente.id
